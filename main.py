b0d713ccf499c61d0b05311f182aada493a193af
Gwenhael <ltgwenhael2003@gmail.com>
2025-07-09 20:51:55 +0200

    Reorganisation des commentaires de main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import random
 
-# Générer un numéro entre 1 & 6 comme un dé
-
+# Génère un nombre aléatoire entre 1 et 6 pour simuler un lancer de dé
 random_integer = random.randint(1, 6)
 print(f"Résultat du dé: {random_integer}")
 
-# Choisir un fruit à partir d'un tableau
-
-# Créer un tableau
-fruit = ["pomme", "orange", "cerise", "poire"]
-# Créer une variable fruit sélectionné = choix aleatoire du tableau fruit
-selected_fruit = random.choice(fruit)
-# Afficher le texte + le résultat du fruit aléatoire
-print(f"Fruit choisi: {selected_fruit}")
+# Sélectionne aléatoirement un fruit dans une liste
+fruits = ["pomme", "orange", "cerise", "poire"]  # Déclaration de la liste des fruits
+selected_fruit = random.choice(fruits)  # Choix aléatoire d'un fruit dans la liste
+print(f"Fruit choisi: {selected_fruit}")  # Affichage du fruit sélectionné
